8218cf0c2b0fd625707aae0a2fdd8a9d68d7c80d
flaviolunaferreira <thecoyote@Estudo>
2021-07-28 09:35:24 -0300

    Iniciando projeto

---
+++ src/main/java/the/coyote/bookstory/BookstoryApplication.java
@@ -0,0 +1,13 @@
+package the.coyote.bookstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/the/coyote/bookstory/BookstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package the.coyote.bookstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
